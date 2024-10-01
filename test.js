3f954610a58305bae7c5504886e977f143789483
Errorist79 <klarendon@outlook.com>
2024-10-01 21:19:57 +0300

    Add TypeScript declaration files and update build process

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const { initializeWasm, getAddressFromSignature } = require('./dist/js/index');
 async function runTest() {
   await initializeWasm();
 
-  const testSignature = 'test_signature';
+  const testSignature = 'sign1ydvx8j84v6v6460jpp9ynw2243er7fc465tjlh5q73xg2ux9kqp8qlzm5vg0palcavs5dfqeg90z3fqvs9mzqmmuqelaqzzxqqdyxqc8awh6a27yfgqskatyxgxq9ev2jt3zsva6h5ke3pan2ygqsqshpg9zle6fa7tlwvwcam4pfm4xrqdcjkdmkmcyze78j40cah9r8gm3qnk833l';
 
   try {
     const address = getAddressFromSignature(testSignature);
